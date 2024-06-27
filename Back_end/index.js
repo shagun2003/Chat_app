806a6650e701d6620305c2e80efeb44b34d5fea7
Shagun_Singh <134138604+shagun2003@users.noreply.github.com>
2024-06-27 17:38:26 +0530

    Update index.js

--- Back_end/index.js
+++ Back_end/index.js
@@ -1,47 +1,52 @@
-const http=require("http");
-const express =require("express");
+const http = require("http");
+const express = require("express");
 const cors = require("cors");
 const socketIO = require("socket.io");
 
-const app=express();
-const port=process.env.PORT ;
+const app = express();
+const port = process.env.PORT; 
 
+const users = {};
 
-const users=[{}];
 
 app.use(cors({
-  origin:"https://chat-app-wwqf.vercel.app" ,
+  origin: "https://chat-app-wwqf.vercel.app", 
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type"],
   credentials: true
 }));
-app.get("/",(req,res)=>{
-    res.send("HELL ITS WORKING");
-})
-const server=http.createServer(app);
 
-const io=socketIO(server);
-
-io.on("connection",(socket)=>{
-console.log("New Connection");
- socket.on('joined',({user})=>{
-          users[socket.id]=user;
-          console.log(${user} has joined );
-          socket.broadcast.emit('userJoined',{user:"Admin",message:${users[socket.id]} has  joined});
-          socket.emit('welcome',{user:"Admin",message:Welcome to the chat ${users[socket.id]}});
-         })
-      socket.on('message',({message,id})=>
-    {
-          io.emit('sendmessage',{user:users[id],message,id})
-    })
-    socket.on('disconnect',()=>
-    {
-        socket.broadcast.emit('leave',{user:"Admin",message:${users[socket.id]} has  left})
-         console.log('User left')
-    })
+// Simple route for testing
+app.get("/", (req, res) => {
+  res.send("HELLO IT'S WORKING");
 });
 
+// Create HTTP server and setup Socket.IO
+const server = http.createServer(app);
+const io = socketIO(server);
 
-server.listen(port,()=>{
-    console.log(Working);
-}) 
+io.on("connection", (socket) => {
+  console.log("New Connection");
+
+  socket.on('joined', ({ user }) => {
+    users[socket.id] = user;
+    console.log(`${user} has joined`);
+    socket.broadcast.emit('userJoined', { user: "Admin", message: `${users[socket.id]} has joined` });
+    socket.emit('welcome', { user: "Admin", message: `Welcome to the chat ${users[socket.id]}` });
+  });
+
+  socket.on('message', ({ message, id }) => {
+    io.emit('sendmessage', { user: users[id], message, id });
+  });
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('leave', { user: "Admin", message: `${users[socket.id]} has left` });
+    console.log('User left');
+    delete users[socket.id]; // Clean up user from list
+  });
+});
+
+// Start the server
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
