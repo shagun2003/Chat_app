2dd11f6147c27bbc03a6db5591bd3606d4b36c4b
Shagun_Singh <134138604+shagun2003@users.noreply.github.com>
2024-07-10 08:13:48 +0530

    Update index.js

--- BACK_END/index.js
+++ BACK_END/index.js
@@ -12,20 +12,9 @@ dotenv.config();
 const users=[{}];
 
 app.use(cors());
-
-const __dirname1=path.resolve();
-if(process.env.NODE_ENV==='production')
-{
-    app.use(express.static(path.join(__dirname1,'/ccat/build')));
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname1,'ccat','build','index.html'));
-    })
-}
-else{
-    app.get("/",(req,res)=>{
+  app.get("/",(req,res)=>{
         res.send("HELL ITS WORKING");
     })
-}
 const server=http.createServer(app);
 
 const io=socketIO(server);
